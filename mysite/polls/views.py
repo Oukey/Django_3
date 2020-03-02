a5570681ce7d59e5b6d07f04c0d354e1a5cdd3f2
Oukey <oukey311@gmail.com>
2020-03-02 21:22:51 +0400

    Добавлен шаблон results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,10 +4,6 @@ from .models import Question, Choice
 from django.urls import reverse
 
 
-# from django.http import Http404
-# from django.template import loader
-
-
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # template = loader.get_template('polls/index.html')
@@ -21,8 +17,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
